232433bec6e1d23f224c3976aaaf5820253f9bb1
Mohamed Ikhlau Ali <ikhlaau@gmail.com>
2024-06-11 21:28:43 +0500

    requiremtn

--- bml_payment_gateway/api.py
+++ bml_payment_gateway/api.py
@@ -1,44 +1,44 @@
-from django.urls import include, path
-from App.views import *
-from rest_framework.routers import DefaultRouter,SimpleRouter
+# from django.urls import include, path
+# from App.views import *
+# from rest_framework.routers import DefaultRouter,SimpleRouter
 
 
-router = DefaultRouter()
-router.register(r'auth', AuthViewSet, basename='auth')
-router.register(r'game', GameViewSet, basename='game')
-router.register(r'store', StoreViewSet, basename='store')
-# router.register(r'designations', DesignationsViewSet, basename='designations')
-# router.register(r'employees', EmployeesViewSet, basename='employees')
-# router.register(r'employee_accounts', EmployeeAccountViewSet, basename='employee_accounts')
-# router.register(r'allowances', AllowanceViewSet, basename='allowances')
-# router.register(r'employee_allowances', EmployeeAllowanceViewSet, basename='employee_allowances')
-# router.register(r'attendance_types', AttendanceTypeViewSet, basename='attendance_types')
-# router.register(r'employee_leaves', EmployeeLeaveViewSet, basename='employee_leaves')
-# router.register(r'employee_files', EmployeeFileViewSet, basename='employee_files')
-# router.register(r'shifts', ShiftViewSet, basename='shifts')
-# router.register(r'employee_attendances', EmployeeAttendanceViewSet, basename='employee_attendances')
-# router.register(r'my', MyViewSet, basename='my')
-# router.register(r'holiday', HolidayViewSet, basename='holiday')
-# router.register(r'salary', SalaryViewSet, basename='salary')
-# router.register(r'loan', EmployeeLoanViewSet, basename='loan')
-# router.register(r'project', ProjectViewSet, basename='project')
-
-
-
+# router = DefaultRouter()
+# router.register(r'auth', AuthViewSet, basename='auth')
+# router.register(r'game', GameViewSet, basename='game')
+# router.register(r'store', StoreViewSet, basename='store')
+# # router.register(r'designations', DesignationsViewSet, basename='designations')
+# # router.register(r'employees', EmployeesViewSet, basename='employees')
+# # router.register(r'employee_accounts', EmployeeAccountViewSet, basename='employee_accounts')
+# # router.register(r'allowances', AllowanceViewSet, basename='allowances')
+# # router.register(r'employee_allowances', EmployeeAllowanceViewSet, basename='employee_allowances')
+# # router.register(r'attendance_types', AttendanceTypeViewSet, basename='attendance_types')
+# # router.register(r'employee_leaves', EmployeeLeaveViewSet, basename='employee_leaves')
+# # router.register(r'employee_files', EmployeeFileViewSet, basename='employee_files')
+# # router.register(r'shifts', ShiftViewSet, basename='shifts')
+# # router.register(r'employee_attendances', EmployeeAttendanceViewSet, basename='employee_attendances')
+# # router.register(r'my', MyViewSet, basename='my')
+# # router.register(r'holiday', HolidayViewSet, basename='holiday')
+# # router.register(r'salary', SalaryViewSet, basename='salary')
+# # router.register(r'loan', EmployeeLoanViewSet, basename='loan')
+# # router.register(r'project', ProjectViewSet, basename='project')
 
 
 
 
-# app_name = 'bsauth'
 
 
-urlpatterns = [
-	# path('login', login_view, name = 'login_view'),
-	# path('change_password', change_password, name = 'change_password'),
-	# path('components/<page_id>', get_page_components, name = 'get_page_components'),
-	# path('roles/', views.view_roles, name = 'view_roles'),
-	# path('roles/', views.view_roles, name = 'view_roles'),
-	# path('role/<id>', views.view_roles, name = 'view_roles_id'),
-	]
 
-urlpatterns += router.urls
+# # app_name = 'bsauth'
+
+
+# urlpatterns = [
+# 	# path('login', login_view, name = 'login_view'),
+# 	# path('change_password', change_password, name = 'change_password'),
+# 	# path('components/<page_id>', get_page_components, name = 'get_page_components'),
+# 	# path('roles/', views.view_roles, name = 'view_roles'),
+# 	# path('roles/', views.view_roles, name = 'view_roles'),
+# 	# path('role/<id>', views.view_roles, name = 'view_roles_id'),
+# 	]
+
+# urlpatterns += router.urls
--- bml_payment_gateway/urls.py
+++ bml_payment_gateway/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('bml_payment_gateway.api')),
+    # path('api/', include('bml_payment_gateway.api')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
