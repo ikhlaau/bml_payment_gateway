f06ccd4b0770de44a92b1b98373eec4206b5b211
Mohamed Ikhlau Ali <ikhlaau@gmail.com>
2024-06-19 09:24:00 +0500

    pam

--- intergration/models.py
+++ intergration/models.py
@@ -4,13 +4,10 @@ from django.db import models
 
 
 class Shopify(models.Model):
-    number = models.CharField(max_length=10,null=False,blank=False)
-    secret = models.CharField(max_length=10,null=False,blank=False)
-    rateplan = models.CharField(max_length=10,null=False,blank=False)
-    name = models.CharField(max_length=250,null=True)
-    sex = models.CharField(max_length=6,null=True)
-    dob = models.DateField(null=True)
-    # user = models.OneToOneField(User,on_delete=models.SET_NULL,null=True,blank=True)
+    webhook_secret = models.CharField(max_length=250,null=False,blank=False)
+    api_key = models.CharField(max_length=250,null=False,blank=False)
+    shop_name = models.CharField(max_length=250,null=True)
+    password = models.CharField(max_length=250,null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     def __str__(self):
